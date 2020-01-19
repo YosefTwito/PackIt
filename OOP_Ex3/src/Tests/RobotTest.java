0eca9500d7cc93e065a6caa19706c03b9fa4f3cd
YosefTwito <yosef@LAPTOP-IAVTIVD5>
2020-01-19 16:10:04 +0200

    .

---
+++ OOP_Ex3/src/Tests/RobotTest.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RobotTest {
+
+	@Test
+	void testRobotIntIntIntPoint3DDoubleInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
