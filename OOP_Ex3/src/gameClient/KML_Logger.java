ca26bf689e5a4ed9205b44fefddf39e66fe23b7c
eldar <eldartakach@gmail.com>
2020-01-19 18:01:53 +0200

    kml start

---
+++ OOP_Ex3/src/gameClient/KML_Logger.java
@@ -0,0 +1,15 @@
+package gameClient;
+/**
+ * this class enables exporting the game, including the robots and fruits to an KML file
+ * which can be loaded to Google Maps.
+ * On Maps you can view the game on a real map.
+ * @author Eldar
+ *
+ */
+public class KML_Logger {
+
+	
+	public static void main(String[] args) {
+	
+	}
+}
